3cad7a4d4c6948c55f5ab887ff5f62c4be9cf4b6
Juhyeon Lee <81895590+gogori6565@users.noreply.github.com>
2024-07-21 21:47:31 +0900

    [FEAT] health Check API 구현

--- src/main/java/com/core/book/api/HealthCheckController.java
+++ src/main/java/com/core/book/api/HealthCheckController.java
@@ -1,9 +1,14 @@
 package com.core.book.api;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HealthCheckController {
 
+    @GetMapping("health")
+    public String healthCheck(){
+        return "OK";
+    }
 
 }
