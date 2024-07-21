98d488c95c3088f004588872f5d111123825c580
rhkr8521 <rhkr8521@gmail.com>
2024-07-21 20:22:21 +0900

    [SETTING] 프로젝트 파일 등록

---
+++ src/main/java/com/core/book/api/HealthCheckController.java
@@ -0,0 +1,9 @@
+package com.core.book.api;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HealthCheckController {
+
+
+}
---
+++ src/main/java/com/core/book/common/response/ErrorStatus.java
@@ -0,0 +1,32 @@
+package com.core.book.common.response;
+
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+
+@Getter
+@RequiredArgsConstructor(access = AccessLevel.PROTECTED)
+
+public enum ErrorStatus {
+    /**
+     * 400 BAD_REQUEST
+     */
+    VALIDATION_REQUEST_MISSING_EXCEPTION(HttpStatus.BAD_REQUEST, "요청값이 입력되지 않았습니다."),
+
+    /**
+     * 404 NOT_FOUND
+     */
+
+    /**
+     * 500 SERVER_ERROR
+     */
+    ;
+
+    private final HttpStatus httpStatus;
+    private final String message;
+
+    public int getStatusCode() {
+        return this.httpStatus.value();
+    }
+}
