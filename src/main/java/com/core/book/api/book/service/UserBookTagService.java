2ef2f904042be6a1098b5febc1a3d5cad030fb79
Juhyeon Lee <81895590+gogori6565@users.noreply.github.com>
2025-02-01 23:02:34 +0900

    [FIX] 태그 일부 삭제 처리 코드 구현

--- src/main/java/com/core/book/api/book/service/UserBookTagService.java
+++ src/main/java/com/core/book/api/book/service/UserBookTagService.java
@@ -19,28 +19,43 @@ import java.util.List;
 public class UserBookTagService {
     private final UserBookTagRepository userBookTagRepository;
 
+    // id로 태그 entity 가져오기
+    public UserBookTag findUserBookTag(int tagId){
+        return userBookTagRepository.findById(tagId).
+                orElseThrow(() -> new NotFoundException(ErrorStatus.TAG_NOT_FOUND_EXCPETION.getMessage()));
+    }
+
     // 태그 수정 메서드
     public void updateUserBookTag(List<UserBookTagDTO> tagList, Book book, ReadBooks readBooks, ReviewArticle reviewArticle){
         if (tagList != null) {
             for (UserBookTagDTO tagDTO : tagList) {
                 if (tagDTO == null) continue;  // null 태그가 있을 경우 건너뛰기
+
+                // 태그 삭제 -> 태그 이름이 null 인 경우
+                if(tagDTO.getTag() == null){
+                    UserBookTag existTag = findUserBookTag(tagDTO.getTagId());
+                    userBookTagRepository.delete(existTag);
+                    continue;
+                }
                 BookTag tagEnum = BookTag.fromDescription(tagDTO.getTag());
 
-                if (tagDTO.getTagId() == 0) { // 태그 새로 저장
+                // 태그 새로 저장 -> 태그 id가 0인 경우
+                if (tagDTO.getTagId() == 0) {
 
                     UserBookTag newUserBookTag = tagDTO.toEntity(book, tagEnum, readBooks, reviewArticle);
                     userBookTagRepository.save(newUserBookTag);
 
-                } else { // 태그 수정
-                    UserBookTag existTag = userBookTagRepository.findById(tagDTO.getTagId()).
-                            orElseThrow(() -> new NotFoundException(ErrorStatus.TAG_NOT_FOUND_EXCPETION.getMessage()));
+                } else { // 태그 수정 -> 태그 id가 있는 경우
+                    UserBookTag existTag = findUserBookTag(tagDTO.getTagId());
 
-                    // 1) 태그 이름 변경 O -> 데이터 변경 (변경 X -> 그대로 유지)
+                    // 태그 이름이 변경되었다면 수정
                     if (tagEnum.getId() != existTag.getTag()) {
 
                         UserBookTag updatedTag = tagDTO.update(existTag, tagEnum);
                         userBookTagRepository.save(updatedTag);
                     }
+                    
+                    // 태그 이름이 그대로라면 변경 없음
                 }
             }
         }
--- src/main/java/com/core/book/api/bookshelf/controller/BookshelfController.java
+++ src/main/java/com/core/book/api/bookshelf/controller/BookshelfController.java
@@ -125,7 +125,8 @@ public class BookshelfController {
             summary = "'읽은 책' 책장 등록 API",
             description = "'읽은 책' 책장에 선택한 책을 상세 정보와 함께 등록합니다. (읽은 책 상세 정보 : 날짜(required)/평점/태그/한줄평)" +
                     "\n- 만약 태그 전체가 입력되지 않았을 경우 : \"userBookTagList\": null" +
-                    "\n- 태그는 최대 5개까지로, id와 태그이름을 같이 보내주세요. 등록의 경우, 태그 id는 0으로 보내시면 됩니다(반드시 0)"
+                    "\n- 태그는 최대 5개까지로, id와 태그이름을 같이 보내주세요. 등록의 경우, 태그 id는 0으로 보내시면 됩니다(반드시 0)" +
+                    "\n- 변경된 태그 요청 방식에 대한 상세 설명 : https://www.notion.so/api-v1-bookshelf-read-id-1a95d7dcdd5f42ad82aa5bc18e5588a9?pvs=4 해당 노션 설명 참고해주세요"
     )
     @ApiResponses({
             @io.swagger.v3.oas.annotations.responses.ApiResponse(responseCode = "200", description = "책장 등록 성공"),
@@ -179,7 +180,9 @@ public class BookshelfController {
     @Operation(
             summary = "'읽은 책' 상세 정보 수정 API",
             description = "'읽은 책' 책장에서 선택한 책의 상세 정보를 수정합니다. (읽은 책 상세 정보 : 날짜(required)/평점/태그/한줄평)" +
-                    "\n- 태그는 최대 5개까지로, id와 태그이름을 같이 보내주세요. 수정의 경우, 기존에 있던 id는 순서 그대로, 새로 추가된 태그는 id를 0으로 보내시면 됩니다"
+                    "\n- 태그는 최대 5개까지로, id와 태그이름을 같이 보내주세요."  +
+                    "\n- 수정의 경우, 기존에 있던 id는 순서 그대로, 새로 추가된 태그는 id를 0으로 보내시면 됩니다. 만약 기존에 있던 태그가 일부 삭제된 경우, id는 그대로, \"tag\": null 로 보내주시면 됩니다." +
+                    "\n- 변경된 태그 요청 방식에 대한 상세 설명 : https://www.notion.so/api-v1-bookshelf-read-id-1a95d7dcdd5f42ad82aa5bc18e5588a9?pvs=4 해당 노션 설명 참고해주세요"
     )
     @ApiResponses({
             @io.swagger.v3.oas.annotations.responses.ApiResponse(responseCode = "200", description = "책장 등록 성공"),
