c0e2f5d461179eceb67ea725afdd6ee56d25601c
Juhyeon Lee <81895590+gogori6565@users.noreply.github.com>
2025-02-05 00:27:47 +0900

    [FEAT] 책 정보 자세히보기 API - Best 5 태그 기능 구현

--- src/main/java/com/core/book/api/article/repository/ArticleRepository.java
+++ src/main/java/com/core/book/api/article/repository/ArticleRepository.java
@@ -6,7 +6,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
-import org.springframework.data.jpa.repository.EntityGraph;
 
 import java.util.List;
 
@@ -17,4 +16,6 @@ public interface ArticleRepository extends JpaRepository<Article, Long> {
 
     Page<Article> findByTypeIn(Iterable<ArticleType> types, Pageable pageable);
 
+    // 특정 ISBN을 가진 최신 게시글 5개 가져오기
+    List<Article> findByBookIsbnOrderByCreatedAtDesc(String isbn, Pageable pageable);
 }
