3ffc323553884ae0ac87554c9ab90be2ffed36b9
rhkr8521 <rhkr8521@gmail.com>
2025-05-28 16:05:06 +0900

    [REFAC] CI/CD 코드 업데이트

--- src/main/java/com/core/book/common/config/swagger/SwaggerConfig.java
+++ src/main/java/com/core/book/common/config/swagger/SwaggerConfig.java
@@ -40,7 +40,7 @@ public class SwaggerConfig {
         SecurityRequirement refreshTokenRequirement = new SecurityRequirement().addList(refreshTokenHeader);
 
         Server server = new Server();
-        server.setUrl("https://moongeul.kro.kr");
+        server.setUrl("/");
 
         return new OpenAPI()
                 .info(new Info()
